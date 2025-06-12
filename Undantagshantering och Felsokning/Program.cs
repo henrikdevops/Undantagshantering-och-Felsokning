90aaa05e882c1e36e5a6805c6c2ee25f6e66b08b
henrikdevops <henrik.jansson19872@gmail.com>
2025-06-12 11:11:29 +0200

    Update Program.cs

--- Undantagshantering och Felsokning/Program.cs
+++ Undantagshantering och Felsokning/Program.cs
@@ -28,9 +28,9 @@ namespace Uppgift_4
                     Console.WriteLine("\tDet inmatade talet " + tal + " är för stort, försök igen."); // + efter tal /Rödigerar felaktig syntax
                 }
 
+                if (tal == speltal)
                 {
-                    if (tal == speltal)
-                        Console.WriteLine("\tGrattis, du gissade rätt!");
+                    Console.WriteLine("\tGrattis, du gissade rätt!");
                     spela = true; //Programmet ska stängas av när talet är rätt gissat!
                 }
             }
