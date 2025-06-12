c9b5a4ee0812282eb97242773dfc76ac2b9ceed6
henrikdevops <henrik.jansson19872@gmail.com>
2025-06-12 11:56:15 +0200

    Update Program.cs

--- Undantagshantering och Felsokning/Program.cs
+++ Undantagshantering och Felsokning/Program.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Uppgift_4
+namespace Undantagshantering_Felsokning
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
